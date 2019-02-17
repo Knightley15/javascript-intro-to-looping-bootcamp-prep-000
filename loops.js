4d03f54a7d7b7c16bec71034bcbc1450e0479c90
Juliette Knightley <knightleyjb@gmail.com>
2019-02-17 22:28:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
   if (i === 1) {
-    array.push("I am 1 strange loop.")
+    array.push(`I am 1 strange loop.`)
   } else {
-  array.push("I am ${i} strange loops.")
+  array.push(`I am ${i} strange loops.`)
   }
   }
   return array
@@ -26,7 +26,7 @@ function incrementVariable() {
 }
 
 do{
-  console.log ('I run once regardless;');
+  console.log (`I run once regardless;`);
   } while (incrementVariable() < num);
 }
 
